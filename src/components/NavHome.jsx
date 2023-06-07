e39fbdfe2d55e9b7bdb7e4909a23ccb584804cd4
Christian Gómez <cgomezz.dev@gmail.com>
2023-06-07 20:08:21 +0200

    css nav and home

--- src/components/NavHome.jsx
+++ src/components/NavHome.jsx
@@ -4,12 +4,15 @@ import { Link } from "react-router-dom";
 
 function NavHome() {
   return (
-    <div>
-      <Navbar bg="light" variant="light">
+    <div className="home">
+      <Navbar className="px-4" bg="light" variant="secondary" expand="lg">
         <Container>
           {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/" href="#home">Home</Nav.Link>
+          </Nav>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/ex1" href="#ex1">Ex1</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NavHome from "../components/NavHome";
+import { Container } from "react-bootstrap";
 
 function Home() {
-  return <div>
+  return <>
     <NavHome/>
     <h1>Welcome to the Home Page</h1>
-  </div>;
+  </>;
 }
 
 export default Home;
